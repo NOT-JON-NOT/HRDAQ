42485e78f3361ed02469050aca98aacb7aa02332
N0T-J0N <73200505+N0T-J0N@users.noreply.github.com>
2024-06-22 11:16:40 -0400

    Delete HRDAQ directory

--- HRDAQ/HRDAQ_EEPROM_INIT/HRDAQ_EEPROM_INIT.ino
+++
@@ -1,8 +0,0 @@
-#include <EEPROM.h>
-
-/*
- * Initializes address 0 of EEPROM in HRDAQ. Should only be run once, after assembly
- * address 0 will be used for the naming of each csv file in the sd card
- */
-void setup(){EEPROM.write(0, 1000);}
-void loop(){}
